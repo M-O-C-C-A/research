f1151d201e09170154d4c5e387b7d30f50d04822
M-O-C-C-A <moccetta@live.com>
2026-03-20 12:00:44 +0100

    Fixing hydration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
+      suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col bg-zinc-950 font-sans selection:bg-zinc-500/30">
         <ConvexClientProvider>
